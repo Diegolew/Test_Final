6a6b0e0a02d2736a60ab7cc7f02c91018fe741ae
Diegolew <diegolewensztain2@gmail.com>
2024-06-10 14:42:37 -0400

    Declaracion de funciones y creacion de una funcion

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//algo
 #include <iostream>
 #include <map>
 #include <vector>
@@ -6,9 +5,20 @@
 
 using namespace std;
 
+void Add(string date, string event);
+void DelEvent(string date, string event);
+void DelDate(string date);
+void Find(string date);
+void Print();
+    
 map<string, vector<string>> db;
 
 int main(){
+string command, date, description;
 
-    return 0;
+return 0;
+}
+
+void Add(string date, string event){
+    db[date].push_back(event);   
 }
