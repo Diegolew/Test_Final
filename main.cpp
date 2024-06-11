5adef69be95b7cc60631511a382a28500f1f694b
Diegolew <diegolewensztain2@gmail.com>
2024-06-10 22:56:18 -0400

    Cambios en el main por errores

--- main.cpp
+++ main.cpp
@@ -10,68 +10,84 @@ void DelEvent(string date, string event);
 void DelDate(string date);
 void Find(string date);
 void Print();
-    
-map<string, vector<string>> db;
+ 
+map<string, vector<string>> database;
 
-int main(){
+int main() {
+cout << "Bienvenido\n";
+cout << "Comandos disponibles: Add, Del, Find, Print" << endl;
+cout << "Ingrese un comando: " << endl;
 
-cout<<"Bienvendido a la base de datos"<<endl;
-cout<<"Comandos disponibles: Add, Del, Find, Print"<<endl;
-cout<<"ingrese un comando: "<<endl;
-
-string command, date, description;
-while (getline(cin , command)) {
-        if (command == ("Add")||("add")) {
-        cin >> date >> description;
-        Add(date, description);
-    } else if (command == ("Del")||("del")) {
-        cin >> date >> description;
-        if (description.empty())
-            DelDate(date);
-        else
-           DelEvent(date, description);
-    } else if (command == ("Find")||("find")) {
-        cin >> date;
-        Find(date);
-       } else if (command == ("Print")||("print")) {
-       Print();
+string line;
+while (getline(cin, line)) {
+    string command;
+    size_t pos = line.find(' ');
+    if (pos != string::npos) {
+        command = line.substr(0, pos);
+        line.erase(0, pos + 1);
+    } else {
+        command = line;
+        line = "";
+    }
+    if (command == "Add") {
+        size_t pos = line.find(' ');
+        if (pos != string::npos) {
+            string date = line.substr(0, pos);
+            string event = line.substr(pos + 1);
+            Add(date, event);
+        }
+    } else if (command == "Del") {
+        size_t pos = line.find(' ');
+        if (pos != string::npos) {
+            string date = line.substr(0, pos);
+            string event = line.substr(pos + 1);
+            if (event.empty()) {
+                DelDate(date);
+            } else {
+                DelEvent(date, event);
+            }
+        }
+    } else if (command == "Find") {
+        Find(line);
+    } else if (command == "Print") {
+        Print();
     } else {
         cout << "Unknown command: " << command << endl;
-        return 1; 
+        return 1;
+        }
     }
-}
-return 0;
+    return 0;
 }
 
 void Add(string date, string event){
-    db[date].push_back(event);   
+    database[date].push_back(event);   
 }
 
 void DelEvent(string date, string event){
-    for(int i = 0; i < db[date].size(); i++){
-        if(db[date][i] == event){
-            db[date].erase(db[date].begin() + i);
+    for(int i = 0; i < database[date].size(); i++){
+        if(database[date][i] == event){
+            database[date].erase(database[date].begin() + i);
         }
     }
 }
 
 void DelDate(string date){
-    db.erase(date);
+    database.erase(date);
 }
 
 void Find(string date){
-    if(db.count(date) == 0){
+    if(database.count(date) == 0){
         cout << "Not found" << endl;
     }
     else{
-        for(int i = 0; i < db[date].size(); i++){
-            cout << db[date][i] << endl;
+        for(int i = 0; i < database[date].size(); i++){
+            cout << database[date][i] << endl;
         }
     }
 }
 
 void Print(){
-    for(auto& date_events : db){
+    for(auto& date_events : database){
         string date = date_events.first;
         vector<string>& events = date_events.second;
         
